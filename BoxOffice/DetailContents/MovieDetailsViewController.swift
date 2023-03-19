f23847b680e68b9721da56ea279bf725503e6952
최원석 <cws653@naver.com>
2023-03-19 20:56:17 +0900

    refactor: 스토리보드 뷰컨트롤러 인스턴스 생성 전용 프로토콜 적용
    스토리보드 뷰컨트롤러 인스턴스 생성 전용 프로토콜 적용을 통해 스토리보드 뷰컨트롤러 인스턴스 사용시 불필요한 코드 줄임

--- BoxOffice/DetailContents/MovieDetailsViewController.swift
+++ BoxOffice/DetailContents/MovieDetailsViewController.swift
@@ -19,6 +19,8 @@ final class MovieDetailsViewController: UIViewController, StoryboardBased {
     
     @IBOutlet private weak var movieDetailTableView: UITableView?
     
+    @IBOutlet private weak var tableView: UITableView?
+    
     // MARK: - view life cycle
     override func viewDidLoad() {
         super.viewDidLoad()
